25835ac1a8cc583df9bea1100fd9dc70bd2468df
jmmarao <jmmarao@gmail.com>
2022-07-21 18:49:52 -0300

    [TASK-4] Saving schema SQL

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE Cliente(
+	id		INTEGER PRIMARY KEY,
+	nome 		VARCHAR(80) NOT NULL,
+	email		VARCHAR(40) NOT NULL,
+	telefone	INTEGER
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
