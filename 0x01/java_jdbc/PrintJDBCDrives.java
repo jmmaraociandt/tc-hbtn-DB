510349d9aea5c9456cc61ef540ae7dae444ecff0
jmmarao <jmmarao@gmail.com>
2022-07-25 12:34:24 -0300

    [TASK-0] Available driver for java project

---
+++ 0x01/java_jdbc/PrintJDBCDrives.java
@@ -0,0 +1,22 @@
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class PrintJDBCDrives {
+    public static void main(String[] args) {
+        System.out.println("List of loaded JDBC drivers");
+
+        for (Enumeration<Driver> driverEnum = DriverManager.getDrivers(); driverEnum.hasMoreElements();) {
+            Driver driver = driverEnum.nextElement();
+            print(driver);
+        }
+    }
+
+    public static void print(Driver driver) {
+        System.out.println("----------------------------------------");
+        System.out.println("Driver name: " + driver.getClass().getName());
+        System.out.println("Major version: " + driver.getMajorVersion());
+        System.out.println("Minor version: " + driver.getMinorVersion());
+        System.out.println("----------------------------------------");
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Bootcamp: CI&T (Java)
+
+IDE used: [IntelliJ IDEA](https://www.jetbrains.com/idea/)
+Java Development Kit used: [11](https://www.oracle.com/java/technologies/downloads/)
+
+## Bootcamp schedule (content from Java)
+- Project 33: [Database introduction](https://github.com/jmmaraociandt/tc-hbtn-DB/0x00)
+
+- Project 34: [JDBC](https://github.com/jmmaraociandt/tc-hbtn-DB/0x01)
+
