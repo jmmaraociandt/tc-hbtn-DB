ce4a34f3b4749534f71c05afb3330d0f28fbf6e7
João Mateus Marão Domingues <CIANDT\jmmarao@lnb035937cps.ciandt.global>
2022-07-27 12:49:46 -0300

    [TASK-2] SQLite online schema

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE Cliente (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+nome TEXT NOT NULL, 
+idade INTEGER NOT NULL, 
+cpf TEXT NOT NULL, 
+rg TEXT NOT NULL 
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
